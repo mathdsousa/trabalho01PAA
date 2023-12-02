36c54da48a00f2cec38c7b8931de8c33ff453fcd
Matheus Sousa <124941736+mathdsousa@users.noreply.github.com>
2023-12-01 22:10:35 -0300

    Add files via upload

---
+++ exponencial.py
@@ -0,0 +1,24 @@
+def logaritmo(num):
+    if (num >= 0 and num <= 9):
+        return 0
+    return 1 + logaritmo(num/10)
+
+def exponencialDC(base, n):
+    if n == 1:
+        return base
+    metade = n//2
+    parcial = exponencialDC(base, metade)
+    if(n % 2 == 0):
+        return parcial * parcial
+    return parcial * parcial * base
+
+def main():
+    # Entrada de valores
+    base, n = int(input()), int(input())
+
+    res = exponencialDC(base, n)
+    log = logaritmo(res)
+
+    print("{} {}".format(res//10**log, log))
+
+main()
