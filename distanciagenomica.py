62628c031368cde2c4d7a24a4d28b6b2851dee14
Matheus Sousa <124941736+mathdsousa@users.noreply.github.com>
2023-12-20 16:54:45 -0300

    update

--- distanciagenomica.py
+++ distanciagenomica.py
@@ -1,16 +1,51 @@
-def interContando():
-    pass
+def intercalaContando(genoma2, inicio, metade, fim, pesos):
+    
+    num_inv = 0
+    i = inicio
+    j = metade
+    k = 0
+    tam = fim - inicio
+    w = [None] * tam
 
-def contarInversoes(genoma2, inicio, fim, pesos):
+    while(i < metade and j < fim):
+        if(pesos[genoma2[i]] <= pesos[genoma2[j]]):
+            w[k] = genoma2[i]
+            k += 1
+            i += 1
+        else:
+            w[k] = genoma2[j]
+            k += 1
+            j += 1
+            num_inv += metade - i
+
+    while(i < metade):
+        w[k] = genoma2[i]
+        k += 1
+        i += 1
+
+    while(j < fim):
+        w[k] = genoma2[j]
+        k += 1
+        j += 1
+
+    k = 0
+
+    for k in range(tam):
+        genoma2[inicio + k] = w[k]
+    
+    return num_inv
+
+def contarInversoesR(genoma2, inicio, fim, pesos):
     numInversoes = 0
-    if(inicio - fim > 1):
-        metade = (inicio + fim // 2)
-        numInversoes += contarInversoes(genoma2, inicio, metade, pesos)
-        numInversoes += contarInversoes(genoma2, metade, fim, pesos)
-        numInversoes += interContando()
+    if(fim - inicio > 1):
+        metade = (inicio + fim) // 2
+        numInversoes += contarInversoesR(genoma2, inicio, metade, pesos)
+        numInversoes += contarInversoesR(genoma2, metade, fim, pesos)
+        numInversoes += intercalaContando(genoma2, inicio, metade, fim, pesos)
     return numInversoes
 
-
+def contarInversoes(genoma2, n, pesos):
+    return contarInversoesR(genoma2, 0, n, pesos)
 
 def preencheLista(num, bool):
     entrada = input()
@@ -32,5 +67,7 @@ def main():
     print(genoma1)
     print(pesos)
     print(genoma2)
+
+    num_inv = contarInversoes(genoma2, num, pesos)
 
 main()
