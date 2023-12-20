a8dc0ccdfc2e2076576316b2b6630aae13e081d4
Matheus Sousa <124941736+mathdsousa@users.noreply.github.com>
2023-12-20 14:56:51 -0300

    update

--- exponencial.py
+++ exponencial.py
@@ -1,22 +1,27 @@
-# Cálculo do logaritmo
+# Universidade Federal de São Carlos
+# Disciplina: Projeto e Análise de Algoritmos
+# Turma: D
+# Alunos: Matheus dos Santos Sousa
+#         Nathália Brasilino Gimenes
+
 def logaritmo(num):
     if (num >= 0 and num <= 9):
         return 0
     return 1 + logaritmo(num/10)
 
-# Cálculo do exponencial usando divisão e conquista
 def exponencialDC(base, n):
-    if n == 1:
-        return base
-    metade = n//2
-    parcial = exponencialDC(base, metade)
-    if(n % 2 == 0):
-        return parcial * parcial
-    return parcial * parcial * base
+    if n == 0:
+        return 1
+    metade = exponencialDC(base, n//2)
+    parcial = metade * metade
+    if(n % 2 == 1):
+        return parcial * base
+    return parcial
 
 def main():
     # Entrada de valores
-    base, n = int(input()), int(input())
+    entrada = input()
+    base, n = [int(numero) for numero in entrada.split(' ')]
 
     res = exponencialDC(base, n)
     log = logaritmo(res)
